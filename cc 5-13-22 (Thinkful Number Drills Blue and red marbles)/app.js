26bd58183580a8173d17502a16dfcbb639e88866
shahx95 <anwaar.shahzaib@gmail.com>
2022-05-13 05:07:32 +0500

    coding challenge and solution for cc 5-13-22

---
+++ cc 5-13-22 (Thinkful Number Drills Blue and red marbles)/app.js
@@ -0,0 +1,49 @@
+/* 
+You and a friend have decided to play a game to drill your statistical intuitions. The game works like this:
+
+You have a bunch of red and blue marbles. To start the game you grab a handful of marbles of each color and put them into the bag, keeping track of how many of each color go in. You take turns reaching into the bag, guessing a color, and then pulling one marble out. You get a point if you guessed correctly. The trick is you only have three seconds to make your guess, so you have to think quickly.
+
+You've decided to write a function, guessBlue() to help automatically calculate whether you should guess "blue" or "red". The function should take four arguments:
+
+the number of blue marbles you put in the bag to start
+the number of red marbles you put in the bag to start
+the number of blue marbles pulled out so far (always lower than the starting number of blue marbles)
+the number of red marbles pulled out so far (always lower than the starting number of red marbles)
+guessBlue() should return the probability of drawing a blue marble, expressed as a float. For example, guessBlue(5, 5, 2, 3) should return 0.6.
+*/
+
+/*
+P: parameters R: return value E: examples P: pseudocode
+
+P: function takes positive integers as input. no other input type will be accepted. the Pulled values must be less than the Start values.
+R: return an float value 
+E: 
+    guessBlue(5, 5, 2, 3) => 0.6
+    guessBlue(5, 7, 4, 3) => 0.2
+    guessBlue(12, 18, 4, 6) => 0.4
+P: 
+    Use the probability formula to get to our result
+    Calculate the total number of blue marbles in the bag
+    Calculate the total number of marbles in the bag
+    Divide total blue marbles with total marbles to get the probability
+    Return the probability
+    Setup a conditional to validate the input. If input is invalid return an  error  
+*/
+
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+    // probability = number of favorable outcomes / total number of possible outcomes
+    
+    if(bluePulled<=blueStart && redPulled<=redStart){
+    
+            let favorableOutcomes = blueStart - bluePulled 
+        //favorableOutcomes is the number of blue marbles in the bag at that moment
+        let totalOutcomes = (blueStart - bluePulled) + (redStart-redPulled) 
+        
+        let probability = favorableOutcomes / totalOutcomes
+        
+        return probability
+    }
+    else{
+        return "Invalid input"
+    }
+  }
