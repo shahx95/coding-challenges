f49813837af3713f5f879dad183a520bd46f8506
shahx95 <anwaar.shahzaib@gmail.com>
2022-06-05 23:44:46 +0500

    coding challenge and solution for 6-5-22

---
+++ cc 6-5-22 (Ones and Zeros)/app.js
@@ -0,0 +1,32 @@
+/*
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+*/
+
+/*
+PREP
+
+P   function accepts an array of 1s and 0s
+R   function returns an integer
+E:
+        Testing: [0, 0, 0, 1] ==> 1
+        Testing: [0, 0, 1, 0] ==> 2
+        Testing: [0, 1, 0, 1] ==> 5
+        Testing: [1, 0, 0, 1] ==> 9
+        Testing: [0, 0, 1, 0] ==> 2
+        Testing: [0, 1, 1, 0] ==> 6
+        Testing: [1, 1, 1, 1] ==> 15
+        Testing: [1, 0, 1, 1] ==> 11
+P:
+    join array values to form a binary number
+    use parseInt to convert binary number to base 10
+    return converted value
+
+*/
+
+//solution
+const binaryArrayToNumber = arr => {
+  
+  return parseInt(arr.join(''),2)
+};
