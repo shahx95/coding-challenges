3ec03ef38e24c7bd21ea95180fd0a8c4ddfd5452
shahx95 <24467345+shahx95@users.noreply.github.com>
2022-06-09 20:36:35 +0500

    coding challenge and solution for cc 6-9-22

---
+++ cc 6-9-22 (Remove the minimum)/app.js
@@ -0,0 +1,29 @@
+/*
+Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.
+*/
+
+/*
+P:  accepts an array of numbers
+R: returns an array
+E:
+        * Input: [1,2,3,4,5], output= [2,3,4,5]
+        * Input: [5,3,2,1,4], output = [5,3,2,4]
+        * Input: [2,2,1,2,1], output = [2,2,2,1]
+P:
+    [2,5,1,7,8]
+    make a copy of the array
+    use Math.min to get the minimum number in the array
+    use indexOf on the minimum number 
+    use splice to delete the element in the derived index
+    return the copied array
+
+*/
+
+//solution
+function removeSmallest(numbers) {
+    let arr = numbers.slice()
+    arr.splice(arr.indexOf(Math.min(...arr)),1)
+    return arr
+  }
