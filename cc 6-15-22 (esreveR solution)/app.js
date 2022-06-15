7af7eace77c2cecd5cf9dd67826eb5587a462a60
shahx95 <24467345+shahx95@users.noreply.github.com>
2022-06-15 22:03:11 +0500

    coding challenge and solution for 6-15-22

---
+++ cc 6-15-22 (esreveR solution)/app.js
@@ -0,0 +1,29 @@
+/*
+Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+(the dedicated builtin(s) functionalities are deactivated)
+*/
+
+/*
+P: parameters R: return value E: examples P: pseudocode
+
+P: array 
+R: array
+E:
+reverse([1,2,3,4,5,6,7,8,9]) => [9, 8, 7, 6, 5, 4, 3, 2, 1]
+P:
+    create a temp array
+    pop from the input array and push in the temp array
+    return the temp array once input array is empty
+*/
+
+//solution
+ 
+function reverse(array) {
+    let temp = []
+    while(array.length){
+        temp.push(array.pop())
+    }
+
+    return temp
+  }
