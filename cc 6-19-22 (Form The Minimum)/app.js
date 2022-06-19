01e543f2017b5a9c8f81ef616adb9de5f491b39e
shahx95 <24467345+shahx95@users.noreply.github.com>
2022-06-19 12:42:39 +0500

    coding challenge and solution for 6-19-22

---
+++ cc 6-19-22 (Form The Minimum)/app.js
@@ -0,0 +1,26 @@
+/* 
+Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+*/
+
+/*
+P: parameters R: return value E: examples P: pseudocode
+
+P: function accepts an array of integers. all integers will be > 0.
+R: function returns an integer
+E: 
+        minValue ({1, 3, 1})  ==> return (13)
+        minValue({5, 7, 5, 9, 7})  ==> return (579)
+        minValue({1, 9, 3, 1, 7, 4, 6, 6, 7}) return  ==> (134679)
+P: 
+    first we will obtain the unique numbers since duplicates are not allowed. we can use the set constructor for this
+    create an array from the set of unique numbers
+    sort the numbers in ascending order and then join them. sorting by ascending will always give us the smallest number
+    return after converting the joined number to a string
+*/
+
+//solution
+function minValue(values){
+    let arr = [...new Set(values)].sort()
+    return +arr.join('')
+  }
