9b1ff7fa9d59417c4abaebaa7a607a0ec994a6b6
shahx95 <24467345+shahx95@users.noreply.github.com>
2022-07-07 22:43:54 +0300

    coding challenge and solution for 7-7-22

---
+++ cc 7-7-22 (Simple beads count)/app.js
@@ -0,0 +1,33 @@
+/*
+Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+B RR B RR B RR B RR B RR B
+Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+If there are less than 2 blue beads return 0.
+*/
+
+/*
+P: parameters R: return value E: examples P: pseudocode
+
+P: integer.
+R: integer.
+E: 
+        countRedBeads(0) => 0
+        countRedBeads(1) => 0
+        countRedBeads(3) => 4
+        countRedBeads(5) => 8
+P:
+    
+    check if n < 2 and return 0
+    else
+        except for the last blue, every blue accompanies two red
+        so subtract 1 blue from the total blue and multiply with 2 to get red
+*/
+
+//solution
+function countRedBeads(n) {
+    if(n<2) { return 0 }
+    else{
+      return (n-1)*2
+    }
+  }
