9003883b8446aab041ea2dab09901dc24cfbc7c9
shahx95 <24467345+shahx95@users.noreply.github.com>
2022-08-16 21:04:51 +0500

    coding challenge and solution for 8-16-22

---
+++ cc 8-16-22 (Find the lucky numbers)/app.js
@@ -0,0 +1,21 @@
+/*
+Write a function filterLucky/filter_lucky() that accepts a list of integers and filters the list to only include the elements that contain the digit 7.
+*/
+
+/*
+P: parameters R: return value E: examples P: pseudocode
+
+P: array of integers. no bad input is expected.
+R: array of integers. 
+E:
+    filterLucky [1,2,3,4,5,6,7,68,69,70,15,17] => [7,70,17]
+
+P:  apply filter array method on the input array 
+        convert number to string and use includes method as a condition
+    return the result of filter method    
+*/
+
+//solution
+var filterLucky=x=>{
+    return x.filter(number => String(number).includes("7"))
+    }
