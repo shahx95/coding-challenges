19b1c6eb860585b7a5f8a40cccc784d8afcd245a
shahx95 <24467345+shahx95@users.noreply.github.com>
2022-08-19 23:20:58 +0500

    coding challenge and solution for 8-19-22

---
+++ cc 8-19-22 (Reverse every other word in the string)/app.js
@@ -0,0 +1,26 @@
+/*
+Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+*/
+
+/*
+P: parameters R: return value E: examples P: pseudocode
+
+P:  a string. empty string is acceptable. any other datatype input is not expected 
+R:  string 
+E:
+    "I really hope it works this time..." => "I yllaer hope ti works siht time..."
+    "Reverse this string, please!" => "Reverse siht string, !esaelp"
+    "   " => ""
+P:
+    split the input string by space
+    apply map on the splitted string array
+        for each element, if the element is in an odd index, reverse the element by using reverse() method
+                           else return the element as it is
+    join the returned map array by space to create a string
+    trim the string returned after joining to remove any whitespaces and return the result                        
+*/
+
+//solution
+function reverse(str){
+    return str.split(" ").map((e,i) => i%2?[...e].reverse().join(""):e).join(" ").trim()
+  }
