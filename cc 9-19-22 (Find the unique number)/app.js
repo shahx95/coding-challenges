e988922adb494ec022d5e399f3a26638a766cd98
shahx95 <24467345+shahx95@users.noreply.github.com>
2022-09-19 00:02:28 +0500

    coding challenge and solution for cc 9-19-22

---
+++ cc 9-19-22 (Find the unique number)/app.js
@@ -0,0 +1,23 @@
+/*
+Write a function called findUnique which returns the only unique number from an array.
+
+All numbers in the unsorted array are present twice, except the one you have to find. The numbers are always valid integer values between 1 and 2147483647, so no need for type and error checking. The array contains at least one number and may contain millions of numbers. So make sure your solution is optimized for speed.
+*/
+
+/*
+P: parameters R: return value E: examples P: pseudocode
+
+P: array of integers
+R: integer
+E:
+        Input: [ 1, 8, 4, 4, 6, 1, 8 ]
+        Expected output: 6
+
+P:  use XOR operation with reduce to get the non duplicate value
+*/
+
+//solution
+
+function findUnique(numbers) {
+    return numbers.reduce((a, b) => a ^ b);
+  }
