f478316a7fee2683d4b1877827c203890d792e44
shahx95 <24467345+shahx95@users.noreply.github.com>
2022-10-18 19:21:34 +0500

    Solution for coding challenge 10-18-22

---
+++ cc 10-18-22 (Comparison)/app.js
@@ -0,0 +1,20 @@
+/*
+You task to pass only this tests :
+
+a == false
+!a == false
+a == !a
+*/
+/*
+P: parameters R: return value E: examples P: pseudocode
+
+P:
+R:
+E:
+P:
+    basically we take advantage of non-strict comparison i.e. use == and not ===
+    non-strict comparison will compare the value but not the type
+*/
+
+//solution
+a = []
