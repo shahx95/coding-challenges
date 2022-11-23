10c238ed2259322031979d3c7db0b35c5f740103
shahx95 <24467345+shahx95@users.noreply.github.com>
2022-11-23 22:51:33 +0500

    Solution for coding challenge 11-23-22

---
+++ cc 11-23-22 (Conditional Rendering &&)/app.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function App() {
+    const [messages, setMessages] = React.useState(["a", "b"])
+    /**
+     * Challenge:
+     * - Only display the <h1> below if there are unread messages
+     */
+
+
+    //solution 
+    return (
+        <div>
+            {
+                messages.length > 0 && 
+                <h1>You have {messages.length} unread messages!</h1>
+            }
+        </div>
+    )
+}
