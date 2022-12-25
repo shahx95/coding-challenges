264b0bcb8a84ce98b8c5a292c2fed9f7e9934f0d
shahx95 <24467345+shahx95@users.noreply.github.com>
2022-12-26 02:17:17 +0500

    Solution for coding challenge 10-25-22

---
+++ cc 12-25-22 (multiple of three)/app.js
@@ -0,0 +1,42 @@
+/*
+Given a positive integer n: 0 < n < 1e6, remove the last digit until you're left with a number that is a multiple of three.
+
+Return n if the input is already a multiple of three, and if no such number exists, return null, a similar empty value, or -1.
+*/
+
+/*
+
+E:
+        1      => null
+        25     => null
+        36     => 36
+        1244   => 12
+        952406 => 9
+
+*/
+
+//solution
+
+/*
+    split the number into an array of strings
+    create a for loop until i is equal to the array length
+        within the for loop create another for loop 
+        if it is divisible by 3, it will return 3, 
+        otherwise it will continue the loop but with the array without the last element, using arr.pop()
+
+*/
+const prevMultOfThree = n => {
+    let arr = n.toString().split('')
+  
+    for(let i = 0; i<arr.length; i++){
+      for(let j = 0; j<arr.length; i++){
+        let sum = +arr.join('')
+        if( sum % 3 === 0) return sum 
+        arr.pop()
+      }
+  
+      return null
+    }
+  
+  }
+  
