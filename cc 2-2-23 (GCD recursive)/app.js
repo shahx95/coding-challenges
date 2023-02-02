3c70c57cdd6a8cdbd0c15f971ec42f81235d56b9
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-02-02 16:52:39 +0500

    Solution to coding challenge 2-2-23

---
+++ cc 2-2-23 (GCD recursive)/app.js
@@ -0,0 +1,27 @@
+// Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+// The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+
+//bruteforce approach
+function mygcd(x, y){
+    for(let i = Math.min(x,y); i > 0; i--){
+      if( x % i === 0 && y % i === 0  ){
+        return i
+      }
+    }
+  }
+  
+  //with recursion
+  function mygcd(a,b){
+    if(b === 0){
+      return a
+    }
+    return mygcd(b, a % b)
+  }
+  
+  
+  
+  console.log(mygcd(30,12),6)
+  // console.log(mygcd(8,9),1)
+  // console.log(mygcd(1,1),1)
