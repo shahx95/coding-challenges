4d03a167b35c6a56cc18e3bd53cf579bf0c5b629
Shah <24467345+shahx95@users.noreply.github.com>
2023-02-07 14:41:41 +0500

    Solution for coding challenge 2-7-23

--- cc 2-7-23 (Closest Square)/app.js
+++ cc 2-7-23 (Closest Square)/app.js
@@ -7,9 +7,9 @@ Your task is to find the nearest square number, closestSquare(n), of a positive 
 function takes a positive integer as an input. no other input type will be accepted.
 return an integer value 
 Examples: 
-    nearestSq(10) => 9
-    nearestSq(111) => 121
-    nearestSq(9999) => 10000
+    closestSquare(10) => 9
+    closestSquare(111) => 121
+    closestSquare(9999) => 10000
 Pseudocode: 
     function takes an integer as an input
     take the square root of the input number and round the result to the nearest integer
