d636b5b2b4c08dcf95377031746303a443798197
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-02-23 12:04:30 +0500

    Solution for coding challenge 2-23-23

---
+++ cc 2-23-23 (Add snippet to code)/app.js
@@ -0,0 +1,23 @@
+//Which snippet could you add to this code to print "food" to the console?
+
+class Animal {
+    static belly = [];
+    eat() {
+      Animal.belly.push('food');
+    }
+  }
+  let a = new Animal();
+  a.eat();
+  console.log(/* Snippet Here */); //Prints food
+
+
+  //solution:  
+  class Animal {
+    static belly = [];
+    eat() {
+      Animal.belly.push('food');
+    }
+  }
+  let a = new Animal();
+  a.eat();
+  console.log(Animal.belly[0]); //Prints food
