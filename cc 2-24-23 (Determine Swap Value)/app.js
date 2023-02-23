276cba66310cade14e3017fc56484cca33223842
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-02-24 00:53:22 +0500

    Solution for coding challenge 2-24-23

---
+++ cc 2-24-23 (Determine Swap Value)/app.js
@@ -0,0 +1,15 @@
+//What is the output of this code? 
+
+// var cat = {name:"Athena"};
+
+// function swap(feline){
+//   feline.name = "Wild";
+//   feline = {name:"Tabby"};
+// }
+
+// swap(cat);
+// console.log(cat.name);
+
+
+//solution
+//cat.name will print "Wild" because {name: "Tabby} is a reassignment of the function's local variable and has no efefct on the cat object
