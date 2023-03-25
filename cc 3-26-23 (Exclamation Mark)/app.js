8c49b748413f6db3ed9ca6582d78adb291b2d107
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-03-26 03:31:44 +0500

    Solution for coding challenge 3-26-23

---
+++ cc 3-26-23 (Exclamation Mark)/app.js
@@ -0,0 +1,19 @@
+// Description:
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// remove("Hi!") == "Hi"
+// remove("Hi!!!") == "Hi!!"
+// remove("!Hi") == "!Hi"
+// remove("!Hi!") == "!Hi"
+// remove("Hi! Hi!") == "Hi! Hi"
+// remove("Hi") == "Hi"
+
+function remove (string) {
+    let stringArr = string.split('')
+    if(stringArr[stringArr.length-1] === '!'){
+      return stringArr.splice(0, string.length - 1).join('')
+    } else {
+      return string;
+    }
+  }
