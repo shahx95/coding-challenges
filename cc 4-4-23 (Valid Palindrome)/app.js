7bef2bde56a7a0b0270e7c061532a3858229cf39
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-04-04 21:29:50 +0500

    Solution for coding challenge 4-4-23

--- cc 4-4-23 (Valid Palindrome)/app.js
+++ cc 4-4-23 (Valid Palindrome)/app.js
@@ -27,15 +27,15 @@ Since an empty string reads the same forward and backward, it is a palindrome.
 function validPalindrome(str){
     //convert to lowercase
     const strLower = str.toLowerCase() //o(n)
-    const alphaNumerical = "abcdefghijklmnopqrstuvwxyz0123456789"
-
+    // const alphaNumerical = "abcdefghijklmnopqrstuvwxyz0123456789"
+    //using charCodeAt instead of alphaNumerical.includes(strLower[left]) for better time complexity
     let left = 0, right = strLower.length-1
     while(left<=right){
-        while(left<=right && ! alphaNumerical.includes(strLower[left])){ //o(n)
+        while(left<=right && !((strLower.charCodeAt(left) >= 97 && strLower.charCodeAt(left) <= 122) || (strLower.charCodeAt(left) >= 48 && strLower.charCodeAt(left) <= 57)) ){ //o(1)
             left++
             // console.log(`left is ${left}`)
         }
-        while(left<=right && ! alphaNumerical.includes(strLower[right])){ //o(n)
+        while(left<=right && !((strLower.charCodeAt(right) >= 97 && strLower.charCodeAt(right) <= 122) || (strLower.charCodeAt(right) >= 48 && strLower.charCodeAt(right) <= 57)) ){ //o(1)
             right--
             // console.log(`right is ${right}`)
         }
