5337dcaf47990a352858f6d32655e29fa57927fe
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-04-11 18:41:16 +0500

    Solution for coding challenge 4-11-23

--- cc 4-11-23 (substitutingVariablesIntoStrings)/app.js
+++ cc 4-11-23 (substitutingVariablesIntoStrings)/app.js
@@ -4,6 +4,13 @@
 
 // solution(5) // should return "Value is 00005"
 
+//simple way
 function solution(value){
     return "Value is " + ("00000" + value).slice(-5)
   }
+
+//using javascript built in function
+function solution2(value){
+    const paddedString = String(value).padStart(5, '0');
+    return `Value is ${paddedString}`
+}  
