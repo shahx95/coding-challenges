9da25508bf7d8c75327d7b0169d840b43c2f33cf
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-04-13 10:33:49 +0500

    Solution for coding challenge 4-13-23

--- cc 4-13-23 (sum-mixed-array)/app.js
+++ cc 4-13-23 (sum-mixed-array)/app.js
@@ -2,9 +2,9 @@
 
 // Return your answer as a number.
 
-// assert.strictEqual(sumMix([9, 3, '7', '3']), 22);
-// assert.strictEqual(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42); 
-// assert.strictEqual(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41); 
+// console.log(sumMix([9, 3, '7', '3']), 22);
+// console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42); 
+// console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41); 
 
 
 function allNums(arr){
