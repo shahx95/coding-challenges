b58a4fb78b5a10eedc5b1f11423cc047a500c8e1
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-05-05 23:11:09 +0500

    Solution for coding challenge 5-5-23

---
+++ cc 5-5-23 (bst)/app.js
@@ -0,0 +1,40 @@
+/*
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+/*
+examples
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+*/
+
+
+//solution
+// [0,2,3,6,8,11,23] target = 6 => 3
+
+function binarySearch(arr,target){
+    let left = 0
+    let right = arr.length-1
+
+    while(left <= right){
+        let midpoint =  Math.round((left + right) / 2) //potential overflow for very large arrays?
+        // console.log(midpoint)
+        if(arr[midpoint] > target){
+            right = midpoint - 1
+        }
+        else if(arr[midpoint] < target){
+            left = midpoint + 1
+        }
+        else return midpoint //number exists
+    }
+
+    return -1
+}
