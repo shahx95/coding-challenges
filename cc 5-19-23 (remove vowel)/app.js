55693a00ccba32d92e878eb2bf877622d87e6b36
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-05-19 21:26:08 +0500

    cc 5-19-23 (remove vowel)

---
+++ cc 5-19-23 (remove vowel)/app.js
@@ -0,0 +1,42 @@
+/*
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+
+*/
+
+
+/* 
+
+ function takes a string only. no other input type is expected
+ return a string value 
+Examples:
+    disemvowel("abracadabra") => 'brcdbr'
+    disemvowel("aloha") => 'lh'
+    disemvowel("rhythm") => 'rhythm'
+Pseudocode:
+    validate the input type
+    split the input string into an array
+    use filter on the split array to create a new array of elements that are consonants 
+    join the filtered array into a string
+    return the filtered array string    
+*/
+
+//solution
+
+function disemvowel(str) {
+    let strToArray = str.split('')  
+    let filteredArray = strToArray.filter( element => "aeiouAEIOU".includes(element)=== false )
+    let arrayToStr = filteredArray.join('')
+    str = arrayToStr
+    return str;
+  }
+
+// single line for when you feel fancy:   
+//      return str.split('').filter( element => "aeiouAEIOU".includes(element)=== false ).join('')
