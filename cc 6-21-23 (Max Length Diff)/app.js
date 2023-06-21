314c9be56272665485c388e354f3cd8016a97a42
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-06-21 18:15:58 +0500

    Solution for coding challenge 6-21-23

---
+++ cc 6-21-23 (Max Length Diff)/app.js
@@ -0,0 +1,30 @@
+/*
+You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+Find max(abs(length(x) − length(y)))
+
+If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+*/
+
+/*
+
+Example:
+        a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+        a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+        mxdiflg(a1, a2) --> 13
+Pseudocode:
+    check if lengths of any array is 0 and return -1
+    we will find the longest string and the shortest string in each array:
+        use map to loop through each string in the array and get their lengths in a new array
+        use Math.max and Math.min to find the longest and shortest values
+    then we will subtract the longest string of one array with the shortest of the other array and vice versa within absolute function. 
+    then return whichever value is larger
+*/
+
+//solution
+function maxLength(arr1,arr2){
+    if(arr1.length===0 || arr2.length===0) {return -1}
+    let arr1Length = arr1.map( element => element.length)
+    let arr2Length = arr2.map( element => element.length)
+    return Math.max(Math.abs(Math.max(...arr1Length)-Math.min(...arr2Length)),Math.abs(Math.max(...arr2Length)-Math.min(...arr1Length)))
+}
