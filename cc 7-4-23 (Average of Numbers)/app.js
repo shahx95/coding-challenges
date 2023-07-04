ab4e18eb35b155eb1ebc1d5a7ddefc8eb8369c7a
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-07-04 21:59:52 +0500

    Solution for coding challenge 7-4-23

---
+++ cc 7-4-23 (Average of Numbers)/app.js
@@ -0,0 +1,40 @@
+/*
+#Get the averages of these numbers
+
+Write a method, that gets an array of integer-numbers and return an array of the averages of each integer-number and his follower, if there is one.
+
+If the array has 0 or 1 values or is null, your method should return an empty array.
+
+*/
+
+
+
+/*
+function takes an array of numbers. numbers can be positive or negative. whole or floats. 
+returns an array of numbers
+
+Example:
+    Input:  [ 1, 3, 5, 1, -10]
+    Output:  [ 2, 4, 3, -4.5]
+
+Pseudocode:
+    create an empty array stored in a variable
+    check input array for null or length <= 1. if true, return empty array.
+    
+    iterate through the input array using a for loop until the 2nd last index
+        for every iteration  take out the average of the element at index and the index+1
+        push the average in the array variable
+    return the array variable after iteration completes    
+*/
+
+
+//solution
+function averages(numbers) {
+    let arr = []
+    if(numbers === null || numbers.length<=1) {return arr}
+    for(let i=0;i<numbers.length-1;i++){
+      let average = (numbers[i]+numbers[i+1])/2
+      arr.push(average)
+    }
+    return arr
+  }
