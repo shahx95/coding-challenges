cd7c0f679d1a9d03ed08b2d026973e6191f3f136
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-07-13 23:29:35 +0100

    Solution for coding challenge 7-13-23

---
+++ cc 7-13-23 (Unique Elements)/app.js
@@ -0,0 +1,28 @@
+/*
+
+Write a function that takes an array and counts the number of each unique element present.
+count(['james', 'james', 'john']) 
+#=> { 'james': 2, 'john': 1}
+
+*/
+
+/*
+E: 
+    count(['james', 'james', 'john']) => { 'james': 2, 'john': 1}
+P:
+    use the reduce method on the input array
+    initialize total counter in the reduce method to an empty object
+    for every iteration check if the element is present in the object
+        if it is then add 1 to its value
+        otherwise create the key and set 1 as its value
+    return the object    
+*/
+
+//solution
+function count(array){
+  
+    return array.reduce( (tally,currentVal) => {
+      tally[currentVal]= (tally[currentVal] || 0) + 1
+      return tally
+    },{})
+  }
