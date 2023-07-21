ee1f6c4d5220f460507978266752e4f8d95c02ff
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-07-22 02:57:56 +0500

    Solution for coding challenge 7-22-23

---
+++ cc 7-21-23 (Sum Mix)/app.js
@@ -0,0 +1,15 @@
+/*
+Write a function called sumArray that takes in an array of numbers as its parameter and returns the sum of all the numbers in the array.
+Make sure to handle edge cases such as an empty array or an array with non-numeric values.
+*/
+
+//examples
+sumArray([1, 2, 3, 4, 5]); // Output: 15
+sumArray([10, 20, 30, 40]); // Output: 100
+sumArray([-1, 0, 1]); // Output: 0
+
+
+//solution
+function sumArray(arr){
+    return arr.reduce((total,current)=>total+current,0) //time compelxity of O(n) space complexity of O(1)
+}
