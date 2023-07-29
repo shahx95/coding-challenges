f38a56810af245b0cdcb05882309da9e03d4b979
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-07-29 21:35:09 +0500

    Solution for coding challenge 7-29-23

---
+++ cc 7-29-23 (Selecting Element)/app.js
@@ -0,0 +1,8 @@
+// Show the different ways of selecting an element from DOM
+
+//Some common ones are:
+document.getElementById("elementId") //selects an element having the id
+document.querySelector("elementId") //selects an element having the id
+document.querySelector("className") //selects the first element having the classname
+document.querySelectorAll("className") //selects all elements having the classname
+ 
