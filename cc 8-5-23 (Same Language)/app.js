b306b6bf12386029b76ed8d5312c5a5426fc6989
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-08-05 03:47:13 +0500

    Solution for coding challenge 8-5-23

---
+++ cc 8-5-23 (Same Language)/app.js
@@ -0,0 +1,35 @@
+/*
+You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return either:
+
+true if all developers in the list code in the same language; or
+false otherwise.
+
+Notes:
+
+The strings representing a given language will always be formatted in the same way (e.g. 'JavaScript' will always be formatted will upper-case 'J' and 'S'
+The input array will always be valid and formatted as in the example above.
+
+*/
+/*
+E:
+        var list1 = [
+        { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+        { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+        { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+        ];
+        
+        isSameLanguage(list1) => true
+P:
+        use the every method on the input array
+            check if every language equals the first language
+            if it does not equal
+                method will return false else true
+*/
+
+//solution
+
+function isSameLanguage(list){
+    return list.every( obj => obj.language === list[0].language)
+}
