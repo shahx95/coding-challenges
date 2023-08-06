01b31fa7329722ce19c45ad1659a4cedb72e49a2
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-08-06 22:28:47 +0500

    Solution for coding challenge 8-6-23

---
+++ cc 8-6-23 (Smallest Val)/app.js
@@ -0,0 +1,21 @@
+/*
+Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
+
+Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+*/
+
+/*
+E:
+    min([1,2,3,4,5], 'value') // => 1
+    min([1,2,3,4,5], 'index') // => 0
+P:
+    find the min using Math.min
+    if toReturn has value, return the min value 
+    else return the index of the min value using indexOf method 
+*/
+
+//solution
+function min(arr, toReturn) {
+    const min = Math.min(...arr)
+    return toReturn === 'value'? min : arr.indexOf(min)
+  }
