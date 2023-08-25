16b23fc6982a2d3a9cca8a61c8c48c55e72d35d2
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-08-25 23:55:42 +0500

    Solution for coding challenge 8-25-23

---
+++ cc 8-25-23 (Kebabish)/app.js
@@ -0,0 +1,38 @@
+/*
+Modify the kebabize function so that it converts a camel case string into a kebab case.
+*/
+/*
+ 
+E:
+    kebabize('camelsHaveThreeHumps') => camels-have-three-humps
+    kebabize('camelsHave3Humps') => camels-have-humps
+P:
+    split the input string into an array of letters
+    apply map on the split array
+        for each letter, check if its Not a Number
+            if true, check if the letter is uppercase and not the first letter of the array
+                if true, if index is 0 just convert it to lower case otherwise convert it to lowercase and add a hyphen before it
+                else convert the letter to lowercase
+    join the map result and store in a result variable             
+    remvoe any preceding hyphen from result string
+    return the result             
+*/
+//solution
+function kebabize(str) {
+    
+    let result = [...str].map((element,index) => {
+      
+      if(isNaN(element)){
+   
+        
+        
+        
+        return element.toUpperCase() === element && index!=0? index===0? element.toLowerCase: `-${element.toLowerCase()}` :element.toLowerCase()
+      }
+   
+    }).join("") 
+    if(result[0]==="-"){
+      return result.slice(1)
+    }
+    else return result
+  }
