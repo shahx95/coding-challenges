2398ef4357953c5a1602183cfc6bba5e223b44b5
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-09-07 23:36:04 +0300

    Solution for coding challenge 9-7-23

--- cc 9-7-23 (Dashetize)/app.js
+++ cc 9-7-23 (Dashetize)/app.js
@@ -1,10 +1,8 @@
-/*
-Given a variable n,
+/*Given a variable n,
 
 If n is an integer, Return a string with dash'-'marks before and after each odd integer, but do not begin or end the string with a dash mark. If n is negative, then the negative sign should be removed.
 
-If n is not an integer, return the string "NaN".
-*/
+If n is not an integer, return the string "NaN".*/
 
 /*
 E:
