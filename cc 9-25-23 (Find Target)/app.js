1765a9ed3553bcbf1d02b6e5035234e49ffbc593
shahx95 <24467345+shahx95@users.noreply.github.com>
2023-09-25 15:39:23 +0500

    Solution for coding challenge 9-25-23

--- cc 9-25-23 (Find Target)/app.js
+++ cc 9-25-23 (Find Target)/app.js
@@ -1,7 +1,7 @@
 // arr = [1,2,3,4,5,6,7]
 // target = 9
 
-function findTarget(arr){
+function findTarget(arr, target){
     let obj = {}
     for(let i=0; i<arr.length; i++){
         if(obj[target-arr[i]]){
